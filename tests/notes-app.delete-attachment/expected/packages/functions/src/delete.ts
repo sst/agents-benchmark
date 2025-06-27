2b5a252f869a98dc2d1d130c751832ad528de608
Frank <frank@sst.dev>
2025-06-27 14:53:40 -0400

    sync

--- tests/notes-app.delete-attachment/expected/packages/functions/src/delete.ts
+++ tests/notes-app.delete-attachment/expected/packages/functions/src/delete.ts
@@ -1,51 +1,40 @@
 import { Resource } from "sst";
 import { Util } from "@notes/core/util";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DeleteCommand, GetCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import {
+  DeleteCommand,
+  GetCommand,
+  DynamoDBDocumentClient,
+} from "@aws-sdk/lib-dynamodb";
 import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
 
 const dynamoDb = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 const s3 = new S3Client({});
 
 export const main = Util.handler(async (event) => {
-  const userId = event.requestContext.authorizer?.iam.cognitoIdentity.identityId;
-  const noteId = event?.pathParameters?.id;
-
-  // First, get the note to check if it has an attachment
-  const getParams = {
+  const params = {
     TableName: Resource.Notes.name,
     Key: {
-      userId,
-      noteId,
+      userId: event.requestContext.authorizer?.iam.cognitoIdentity.identityId,
+      noteId: event?.pathParameters?.id, // The id of the note from the path
     },
   };
 
-  const result = await dynamoDb.send(new GetCommand(getParams));
-  
-  if (!result.Item) {
-    throw new Error("Item not found.");
-  }
+  // First, get the note to check if it has an attachment
+  const result = await dynamoDb.send(new GetCommand(params));
 
-  // If the note has an attachment, delete it from S3
-  if (result.Item.attachment) {
-    const deleteObjectParams = {
+  if (result.Item && result.Item.attachment) {
+    // Delete the attachment from S3
+    const deleteParams = {
       Bucket: Resource.Uploads.name,
-      Key: `private/${userId}/${result.Item.attachment}`,
+      Key: `private/${event.requestContext.authorizer?.iam.cognitoIdentity.identityId}/${result.Item.attachment}`,
     };
 
-    await s3.send(new DeleteObjectCommand(deleteObjectParams));
+    await s3.send(new DeleteObjectCommand(deleteParams));
   }
 
   // Delete the note from DynamoDB
-  const deleteParams = {
-    TableName: Resource.Notes.name,
-    Key: {
-      userId,
-      noteId,
-    },
-  };
-
-  await dynamoDb.send(new DeleteCommand(deleteParams));
+  await dynamoDb.send(new DeleteCommand(params));
 
   return JSON.stringify({ status: true });
 });
